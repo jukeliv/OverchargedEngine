96fd9e93d4c87eb5db86558a602c8bc17eecde52
OverchargedDev <89088882+susyboy23@users.noreply.github.com>
2022-03-05 21:24:55 -0300

    Update HAXE_STUFF.bat

--- !help/HAXE_STUFF.bat
+++ !help/HAXE_STUFF.bat
@@ -1,7 +1,7 @@
 @echo off
 title INSTALL DEPENDENCES
 color 0a
-#Install HaxeFlixel
+Rem Install HaxeFlixel
 haxelib install lime
 haxelib install openfl
 haxelib install flixel
@@ -11,16 +11,16 @@ haxelib install flixel-tools
 haxelib run flixel-tools setup
 haxelib update flixel
 
-#Install dependences
+Rem Install dependences
 haxelib install flixel-ui
 haxelib install hscript
 haxelib install newgrounds
 
-#Git stuff
+Rem Git stuff
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git flixel-addons https://github.com/HaxeFlixel/flixel-addons
 haxelib git extension-webm https://github.com/KadeDev/extension-webm
-#Webm
+Rem Webm
 haxelib install actuate
 haxelib install openfl-webm
 lime rebuild extension-webm windows
